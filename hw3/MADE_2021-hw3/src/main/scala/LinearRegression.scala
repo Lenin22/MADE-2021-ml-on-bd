4ff6385df539f01594342af910b7231683ae7591
Lenin22 <sirazievlenar@mail.ru>
2021-11-03 15:45:23 +0300

    prettified

--- hw3/MADE_2021-hw3/src/main/scala/LinearRegression.scala
+++ hw3/MADE_2021-hw3/src/main/scala/LinearRegression.scala
@@ -1,13 +1,11 @@
-import breeze.linalg.DenseMatrix._
-import breeze.linalg.norm._
 import breeze.linalg._
 import breeze.linalg.csvwrite
-
+import breeze.linalg.DenseMatrix._
+import breeze.linalg.norm._
 import java.io._
 
 object LinearRegression extends App
 {
-  // parameters & vars
   var nIter: Int = 1000
   val theta = DenseVector.ones[Double](4)
   var alpha: Double = 0.005
@@ -33,7 +31,6 @@ object LinearRegression extends App
     ones[Double](10000,1) )
   var xTest: DenseMatrix[Double] = horzcat(csvread(new java.io.File("data/test.csv")),
     ones[Double](1000,1) )
-
   val yTrain = csvread(new java.io.File("data/ytrain.csv"))
 
   var i:Int = 0
@@ -41,9 +38,7 @@ object LinearRegression extends App
   {
     backProp(xTrain, yTrain.toDenseVector)
     val trainLoss: Double = mse(xTrain, yTrain.toDenseVector)
-
     debugger.println(s"Train loss: $trainLoss")
-
     i+=1
   }
 
